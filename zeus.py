2732f9655e10c9014e11750ebc78ac09ed3a573c
Liam Jameson <ljameson@iastate.edu>
2018-07-31 16:43:59 -0500

    Researched best methods for file writing in python and began the conversion from perl to python

--- zeus.py
+++ zeus.py
@@ -1,2 +1,28 @@
-#Init file commit
+#!/usr/bin/env python3
+# zeus.py ---POD format documentation is at the end of this file
 
+#Used to decode the JSON
+import json
+#Used to create a temporary file and write the contents to it
+import os, subprocess, tempfile
+
+
+version = "v0.7"
+
+
+
+
+
+# Boastful startup message
+print ('zeus ' + version)
+print ('\t \"Release the Kraken!\"\n')
+
+
+
+
+##############################################
+# Personal Notes #
+
+# One way to edit the file
+# import subprocess
+# subprocess.call(['vi', 'filename.txt'])
