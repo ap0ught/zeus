4a17a7b61bb91e090b5ae3730433fa453f570f6d
Liam Jameson <ljameson@iastate.edu>
2018-08-03 00:33:30 -0500

    Currently a MVP... Finished adding deployment run/walk functions, did testing and fixed some edge cases, refactored some code so it looks better as well. Next is documentation and further code cleanup if needed.

--- zeus.py
+++ zeus.py
@@ -9,7 +9,7 @@ import subprocess
 import tempfile
 import io
 import collections
-#Used to give system exit and error messages
+#Used to give system exit
 import sys
 
 # General defaults/declarations
@@ -47,18 +47,18 @@ Aliases:
 
 help_list_new = """
 Commands:
-	abort			quit, exit, etc.
-	edit			edit the deplotment plan
+	exit			exit the program
 	help			print this help menu
+	edit			edit the deplotment plan
 	load <file>		load the json deployment plan from the specified path
-	run [lines]		execute the indicated commands, eg. \"1-3,5-10\"
+	run [lines]		execute all OR execute specified commands, eg. \"1-3,5-10\"
 	walk [lines]		same as run, but prompts before each command
-	show			show the command list
+	show			show the current deployment plan and any return codes
 """
 
 # Function Declarations
-def cancel():
-    print(bcolors.WARNING + "WARNING: Aborting Program" + bcolors.ENDC)
+def exit_program():
+    print(bcolors.WARNING + "WARNING: Exiting Program" + bcolors.ENDC)
     exit(0)
 #End of Function
 
@@ -72,7 +72,7 @@ def edit():
             print(bcolors.WARNING + "EXCEPTION: Unkown Exception" + bcolors.ENDC)
             print(sys.exc_info()[0])
     else:
-        print(bcolors.WARNING + "Please Load A File First" + bcolors.ENDC)
+        print(bcolors.WARNING + "Please Load A Deployment Plan" + bcolors.ENDC)
 #End of Function
 
 def help():
@@ -86,7 +86,6 @@ def load(path):
     try:
         deploy_dict = json.loads(io.open(file_path, mode="r", encoding="utf-8").read())
         #For testing purposes only to show that the dict is sucessfully created
-        # print json.dumps(deploy_dict, sort_keys=True, indent=4)
     except ValueError:
         print(bcolors.WARNING + "EXCEPTION: Please Check JSON File Syntax" + bcolors.ENDC)
     except IOError:
@@ -102,32 +101,108 @@ def reset():
     print(bcolors.WARNING + "Deployment Plan Reset" + bcolors.ENDC)
 #End of Function
 
-# TODO
-def run(lines): # execute the indicated commands, eg. \"1-3,5-10\"
+def run(lines):
     global deploy_dict
     global dep_res_dict
-    if not lines:
-        # If there are no specified lines to run, then run them all!
-        # for k,v in deploy_dict.items():
-        for k, v in collections.OrderedDict(sorted(deploy_dict.items())).items():
-            print(bcolors.OKGREEN+ "INFO: Running Command #"+ k +bcolors.ENDC)
-            return_code = subprocess.call(v, shell=True)
-            #If the return_code is not 0 (Meaning a good execution)
-            #Then report the error and add the result to the dep_res_dict
-            if return_code:
-                print(bcolors.FAIL + "ERROR: Command #"+ k +" Failed"+bcolors.ENDC)
-            dep_res_dict[str(k)]=return_code
-        show_return()
+    dep_res_dict = {}
+    #If deploy_dict is empty
+    if not bool(deploy_dict):
+        print(bcolors.WARNING + "Please Load A Deployment Plan" + bcolors.ENDC)
     else:
-        print lines # <type 'str'>
-        print type(lines)
+        if not lines:
+            # If there are no specified lines to run, then run them all!
+            for k, v in collections.OrderedDict(sorted(deploy_dict.items())).items():
+                print(bcolors.OKGREEN+ "INFO: Running Command #"+ k +bcolors.ENDC)
+                return_code = subprocess.call(v, shell=True)
+                #If the return_code is not 0 (Meaning a good execution)
+                #Then report the error and add the result to the dep_res_dict
+                if return_code:
+                    print(bcolors.FAIL + "ERROR: Command #"+ k +" Failed"+bcolors.ENDC)
+                dep_res_dict[str(k)]=return_code
+            show_return()
+        else:
+            line_range = findRange(lines)
+            for x in line_range:
+                if str(x) not in deploy_dict:
+                    print(bcolors.WARNING+ "WARNING: Command #"+ str(x) +" does not exist"+bcolors.ENDC)
+                    dep_res_dict[str(x)]=127
+                else:
+                    print(bcolors.OKGREEN+ "INFO: Running Command #"+ str(x) +bcolors.ENDC)
+                    return_code = subprocess.call(deploy_dict.get(str(x)), shell=True)
+                    #If the return_code is not 0 (Meaning a good execution)
+                    #Then report the error and add the result to the dep_res_dict
+                    if return_code:
+                        print(bcolors.FAIL + "ERROR: Command #"+ str(x) +" Failed"+bcolors.ENDC)
+                    dep_res_dict[str(x)]=return_code
+            show_return()
 #End of Function
 
-# TODO
+def findRange(lines):
+    rangeArr = []
+    splitLines = lines.replace(',', ' ').split()
+    for x in splitLines:
+        if '-' in x:
+            rangeArr = rangeArr + range(int(x.split('-')[0]), int(x.split('-')[1])+1)
+        else:
+            rangeArr.append(int(x))
+    return rangeArr
+#End of Function
+
 def walk(lines):
-    print "This is where i would exec walk(lines)"
-    print lines
-    pass # same as run, but prompts before each command
+    global deploy_dict
+    global dep_res_dict
+    dep_res_dict = {}
+    if not bool(deploy_dict):
+        print(bcolors.WARNING + "Please Load A Deployment Plan" + bcolors.ENDC)
+    else:
+        if not lines:
+            # If there are no specified lines to run, then run them all!
+            for k, v in collections.OrderedDict(sorted(deploy_dict.items())).items():
+                print(bcolors.OKGREEN+ "The next command to run is #"+ k +bcolors.ENDC)
+                print("--> "+v)
+                while True:
+                    var = raw_input(bcolors.WARNING+ "Shall we abort, skip, or continue? [a|s|c]: "+bcolors.ENDC)
+                    if(var == "a" or var == "A"):
+                        prompt_loop()
+                    elif(var == "s" or var == "S"):
+                        break
+                    elif(var == "c" or var == "C"):
+                        print(bcolors.OKGREEN+ "INFO: Running Command #"+ v +bcolors.ENDC)
+                        return_code = subprocess.call(v, shell=True)
+                        if return_code:
+                            print(bcolors.FAIL + "ERROR: Command #"+ k +" Failed. Exiting deployment..."+bcolors.ENDC)
+                        dep_res_dict[str(k)]=return_code
+                        prompt_loop()
+                        break
+                    else:
+                        print(bcolors.WARNING+ "Invalid input... Please try again."+bcolors.ENDC)
+            show_return()
+        else:
+            line_range = findRange(lines)
+            for x in line_range:
+                if str(x) not in deploy_dict:
+                    print(bcolors.WARNING+ "WARNING: Command #"+ str(x) +" does not exist... Skipping"+bcolors.ENDC)
+                    dep_res_dict[str(x)]=127
+                else:
+                    print(bcolors.OKGREEN+ "The next command to run is #"+ str(x) +bcolors.ENDC)
+                    print("--> "+deploy_dict.get(str(x)))
+                    while True:
+                        var = raw_input(bcolors.WARNING+ "Shall we abort, skip, or continue? [a|s|c]: "+bcolors.ENDC)
+                        if(var == "a" or var == "A"):
+                            prompt_loop()
+                        elif(var == "s" or var == "S"):
+                            break
+                        elif(var == "c" or var == "C"):
+                            print(bcolors.OKGREEN+ "INFO: Running Command #"+ str(x) +bcolors.ENDC)
+                            return_code = subprocess.call(deploy_dict.get(str(x)), shell=True)
+                            if return_code:
+                                print(bcolors.FAIL + "ERROR: Command #"+ str(x) +" Failed. Exiting deployment..."+bcolors.ENDC)
+                            dep_res_dict[str(x)]=return_code
+                            prompt_loop()
+                            break
+                        else:
+                            print(bcolors.WARNING+ "Invalid input... Please try again."+bcolors.ENDC)
+            show_return()
 #End of Function
 
 def show():
@@ -135,21 +210,60 @@ def show():
     global deploy_dict
     global dep_res_dict
     if not file_path:
-        print(bcolors.WARNING + "Please Load A File First" + bcolors.ENDC)
+        print(bcolors.WARNING + "Please Load A Deployment Plan" + bcolors.ENDC)
     else:
-        # print io.open(file_path, mode="r", encoding="utf-8").read()
         for k, v in collections.OrderedDict(sorted(deploy_dict.items())).items():
             print "   "+k +" : "+ v
+        if bool(dep_res_dict):
+            show_return()
 #End of Function
 
 def show_return():
     global dep_res_dict
-    print(bcolors.OKGREEN+ "Deployment Return Codes:"+bcolors.ENDC)
-    for k, v in collections.OrderedDict(sorted(dep_res_dict.items())).items():
-        if v:
-            print "   "+k+"R : "+ (bcolors.FAIL+str(v)+bcolors.ENDC)
+    if bool(dep_res_dict):
+        print(bcolors.OKGREEN+ "Deployment Return Codes:"+bcolors.ENDC)
+        for k, v in collections.OrderedDict(sorted(dep_res_dict.items())).items():
+            if v:
+                print "   "+k+"R : "+ (bcolors.FAIL+str(v)+bcolors.ENDC)
+            else:
+                print "   "+k+"R : "+ (bcolors.OKGREEN+str(v)+bcolors.ENDC)
+#End of Function
+
+def prompt_loop():
+    while True:
+        var = raw_input(bcolors.OKGREEN + "zeus--> "+ bcolors.ENDC+" ").split()
+        if(not var):
+            print(bcolors.WARNING + "No Arguments Given, Please Try Again Or Try 'help'" + bcolors.ENDC)
+        elif(var[0] == "help"):
+            help()
+        elif(var[0] == "edit"):
+            edit()
+        elif(var[0] == "exit"):
+            exit_program()
+        elif(var[0] == "load"):
+            if(len(var) == 1):
+                print(bcolors.WARNING + "No Filepath Given" + bcolors.ENDC)
+            else:
+                load(var[1])
+        elif(var[0] == "reset"):
+            reset()
+        elif(var[0] == "run"):
+            if(len(var) == 1):
+                run('')
+            else:
+                run(var[1])
+        elif(var[0] == "walk"):
+            if(len(var) == 1):
+                walk('')
+            else:
+                walk(var[1])
+        elif(var[0] == "show"):
+            show()
+        #Dev function for checking the contents of deployment dictionary
+        elif(var[0] == "test"):
+            print_dict()
         else:
-            print "   "+k+"R : "+ (bcolors.OKGREEN+str(v)+bcolors.ENDC)
+            print(bcolors.FAIL + "Invalid Command: Please Try Again Or Try 'help'"+ bcolors.ENDC)
 #End of Function
 
 # Dev function for checking the contents of the deploy dictionary
@@ -171,7 +285,6 @@ def print_dict():
 print ('\t\t       Zeus ' + version)
 print ('\t\t \"Release the Kraken!\"\n')
 
-
 # Process the command line arguments...
 for x in sys.argv[1:]:
     if(x == '-h' or x == '--help'):
@@ -181,39 +294,8 @@ for x in sys.argv[1:]:
     else:
         sys.exit(bcolors.FAIL + "ERROR: Unrecognized Argument: "+ x + bcolors.ENDC)
 
-
 # Begin Prompt Loop
-while True:
-    var = raw_input(bcolors.OKGREEN + "zeus--> "+ bcolors.ENDC+" ").split()
-    if(not var):
-        print(bcolors.WARNING + "No Arguments Given, Please Try Again Or Try 'help'" + bcolors.ENDC)
-    elif(var[0] == "help"):
-        help()
-    elif(var[0] == "edit"):
-        edit()
-    elif(var[0] == "abort"):
-        cancel()
-    elif(var[0] == "load"):
-        if(len(var) == 1):
-            print(bcolors.WARNING + "No Filepath Given" + bcolors.ENDC)
-        else:
-            load(var[1])
-    elif(var[0] == "reset"):
-        reset()
-    elif(var[0] == "run"):
-        if(len(var) == 1):
-            run('')
-        else:
-            run(var[1])
-    elif(var[0] == "walk"): #TODO
-        walk(var[1])
-    elif(var[0] == "show"):
-        show()
-    #Dev function for checking the contents of deployment dictionary
-    elif(var[0] == "test"):
-        print_dict()
-    else:
-        print(bcolors.FAIL + "Invalid Command: Please Try Again Or Try 'help'"+ bcolors.ENDC)
+prompt_loop()
 
 #------------------------------------------------------------------------------#
 
@@ -241,7 +323,7 @@ while True:
 #           to implement a temporary solution by adding the Version header to the
 #           JSON files. Again this would require a change of plan_deployment.pl
 #           for full implementation
-#       ->
+#       -> Just need to clean up the code a little bit and write some documentation
 
 # July 31st, 2018
 #   Things to Ask Mayfield
@@ -271,17 +353,17 @@ while True:
 ###################
 
 #TODO
-# [] implement run()
+# [X] implement run()
 #   [X] implement run() W/O specified lines
-#   [] implement run() W/ specified lines
+#   [X] implement run() W/ specified lines
 #   [X] implement the return of execution codes
 #   [X] implement the addition of returned execution codes to dictionary
 # ? [] implement the overwrite of the JSON file once everything has been exec
 # ? [] Show the overall JSON file once everything has been ran
-# [] implement walk()
-#   [] implement the Similar things to the above function w/ asking next steps
-# [] implement 'Versioning'
-#   [] implement versioning of different files either by header of new file
+# [X] implement walk()
+#   [X] implement the Similar things to the above function w/ asking next steps
+# ? [] implement 'Versioning'
+# ?  [] implement versioning of different files either by header of new file
 
 
 
@@ -289,19 +371,19 @@ while True:
 
 # [X] Create error handling to replace AnsweRS:ErrorMaker
 # [X] Process command line arguments and print help menu if needed
-# [] Create & process signal handlers for python3
+# [X] Create & process signal handlers for python3
 #     -> Ask Mayfield about this one
-# [] Create a similar Command dispatch table for Zeus execution
+# [X] Create a similar Command dispatch table for Zeus execution
 #     [X] Finish edit()
 #     [X] Finish load(path)
 #     [X] Finish reset()
-#     [] Finish run(lines)
-#     [] Finish walk(lines)
+#     [X] Finish run(lines)
+#     [X] Finish walk(lines)
 #     [X] Finish show()
 # [X] Create Prompt loop
 # [X] Take in JSON file from specified path
-# [] Decode the JSON file to run deployment steps OR run straight from file
-# [] Save the return code of each deployment step
+# [X] Decode the JSON file to run deployment steps OR run straight from file
+# [X] Save the return code of each deployment step
 #     -> Either do this by adding a sub number like "4.1":
 #     -> Or creating a temp JSON file that will have sub sections
 #     -> Maybe think about decoding the JSON into a hashset and then exec that
